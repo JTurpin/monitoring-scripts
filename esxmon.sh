352cb65647f0aefd814a30dd735f7fa1073bc948
Jim Turpin <jim@openwager.com>
2016-07-15 08:03:30 -0600

    updating esmon and setting sh to exec

--- esxmon.sh
+++ esxmon.sh
@@ -12,10 +12,10 @@ while :
 do
 
 	#Let's start with the "easy" one, get the CPU usage
-	cpu1=`snmpget -v 2c -c public 192.168.9.26 HOST-RESOURCES-MIB::hrProcessorLoad.1 -Ov`
-	cpu2=`snmpget -v 2c -c public 192.168.9.26 HOST-RESOURCES-MIB::hrProcessorLoad.2 -Ov`
-	cpu3=`snmpget -v 2c -c public 192.168.9.26 HOST-RESOURCES-MIB::hrProcessorLoad.3 -Ov`
-	cpu4=`snmpget -v 2c -c public 192.168.9.26 HOST-RESOURCES-MIB::hrProcessorLoad.4 -Ov`
+	cpu1=`snmpget -v 2c -c public 192.168.230.5 HOST-RESOURCES-MIB::hrProcessorLoad.1 -Ov`
+	cpu2=`snmpget -v 2c -c public 192.168.230.5 HOST-RESOURCES-MIB::hrProcessorLoad.2 -Ov`
+	cpu3=`snmpget -v 2c -c public 192.168.230.5 HOST-RESOURCES-MIB::hrProcessorLoad.3 -Ov`
+	cpu4=`snmpget -v 2c -c public 192.168.230.5 HOST-RESOURCES-MIB::hrProcessorLoad.4 -Ov`
 
 	#Strip out the value from the SNMP query
 	cpu1=$(echo $cpu1 | cut -c 10-)
@@ -24,7 +24,7 @@ do
 	cpu4=$(echo $cpu4 | cut -c 10-)
 
 	#Now lets get the hardware info from the remote host
-	hwinfo=$(ssh -t root@192.168.9.26 "esxcfg-info --hardware")
+	hwinfo=$(ssh -t root@192.168.230.5 "esxcfg-info --hardware")
 
 	#Lets try to find the lines we are looking for
 	while read -r line; do
@@ -60,21 +60,21 @@ do
 	used=$((kmem - freemem))
 	used=$((used * 100))
 	pcent=$((used / kmem))
-	
+
 	echo "CPU1: $cpu1%"
 	echo "CPU2: $cpu2%"
 	echo "CPU3: $cpu3%"
 	echo "CPU4: $cpu4%"
 	echo "Memory Used: $pcent%"
-	
+
 	#Write the data to the database
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=1 value=$cpu1"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=2 value=$cpu2"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=3 value=$cpu3"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=4 value=$cpu4"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "esxi_stats,host=esxi1,type=memory_usage value=$pcent"
+	curl -i -XPOST 'http://192.168.230.41:8086/write?db=perf_metrics' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=1 value=$cpu1"
+	curl -i -XPOST 'http://192.168.230.41:8086/write?db=perf_metrics' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=2 value=$cpu2"
+	curl -i -XPOST 'http://192.168.230.41:8086/write?db=perf_metrics' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=3 value=$cpu3"
+	curl -i -XPOST 'http://192.168.230.41:8086/write?db=perf_metrics' --data-binary "esxi_stats,host=esxi1,type=cpu_usage,cpu_number=4 value=$cpu4"
+	curl -i -XPOST 'http://192.168.230.41:8086/write?db=perf_metrics' --data-binary "esxi_stats,host=esxi1,type=memory_usage value=$pcent"
 
 	#Wait for a bit before checking again
 	sleep "$sleeptime"
-	
+
 done
